2aa74bb6b60ce911c52bb794f94080a5a8aad139
Tyra Muhlestein <tmuhlestein@xpressbillpay.net>
2019-05-30 19:12:33 -0600

    .then(), .bind(), state\setState(), props - Use as finished example

--- src/components/FamilyChanger.js
+++ src/components/FamilyChanger.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class FamilyChanger extends Component {
-  render() {
+const FamilyChanger = (props) => {
+  
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" onChange={ (e)=> props.update(e.target.value)} disabled={props.allowEdit==="false"}>
         <option value="monospace"> Monospace </option>
         <option value="arial"> Arial </option>
         <option value="courier"> Courier </option>
       </select>
     );
-  }
+
 }
+export default FamilyChanger
--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class ColorChanger extends Component {
-  render() {
+
+// Add `props` as a parameter to the component's function.
+//  Locate the `select` tag, in all four files, and add an `onChange` prop:
+//    The `onChange` should use an arrow function to capture the `event`. 
+//    Inside the arrow function call the `update` prop with the value of the target from the `event`.
+//    Parse Int the value of the target when in `SizeChanger.js`. 
+//  Locate the `select` tag, in `ColorChanger`, `FamilyChanger`, and `SizeChanger`, and add a `disabled` prop:
+//    The `select` element should be `disabled` if `allowEdit` from <b>props</b> is equal to `"false"`.
+//    
+
+const SizeChanger = (props) => {
+ 
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" onChange={ (e) => props.update( parseInt(e.target.value) ) } disabled={ props.allowEdit === "false" }>
         <option value="12"> 12 </option>
         <option value="13"> 13 </option>
         <option value="14"> 14 </option>
       </select>
     );
-  }
 }
+export default SizeChanger
